fddf72ce38a1606f79ac2e927f005b52c8574ab8
Chirag Gandherva <chiraggandherva08@gmail.com>
2024-01-24 17:36:46 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -2,39 +2,58 @@ const invest_ = document.querySelector('#return');
 const years_ = document.querySelector('#years');
 const percentage_ = document.querySelector('#percentage');
 
-invest_.addEventListener('change', () => {
+
+const x = (percentage_.value/0.15 - 52) * 2;
+percentage_.style.background = 'linear-gradient(90deg, rgb(208, 31, 75)' + x + '%, rgb(191, 191, 191)' +  x + '%)';
+
+
+
+invest_.oninput = function() {
     document.querySelector('#rupee').value = `₹${invest_.value}`;
     document.querySelectorAll('#result h2')[0].innerHTML = `₹${(invest_.value) * (years_.value) * 12}`;
 
     const total = invest_.value * (Math.pow(1 + percentage_.value / 1200, 12 * years_.value) - 1) / (percentage_.value / 1200);
     document.querySelectorAll('#result h2')[1].innerHTML = `₹${Math.ceil(total)}`;
+}
+invest_.addEventListener('mousemove', () => {
+    const x = invest_.value;
+    invest_.style.background = 'linear-gradient(90deg, rgb(208, 31, 75)' + x/5000 + '%, rgb(191, 191, 191)' +  x/5000 + '%)';
 })
-
-years_.addEventListener('change', () => {
-    document.querySelector('#years_val').value = `${years_.value} Years`;
-    document.querySelectorAll('#result h2')[0].innerHTML = `₹${(invest_.value) * (years_.value) * 12}`;
-
-    const total = invest_.value * (Math.pow(1 + percentage_.value / 1200, 12 * years_.value) - 1) / (percentage_.value / 1200);
-    document.querySelectorAll('#result h2')[1].innerHTML = `₹${Math.ceil(total)}`;
-})
-
-percentage_.addEventListener('change', () => {
-    document.querySelector('#percentage_val').value = `${percentage_.value}%`;
-    
-    const total = invest_.value * (Math.pow(1 + percentage_.value / 1200, 12 * years_.value) - 1) / (percentage_.value / 1200);
-    document.querySelectorAll('#result h2')[1].innerHTML = `₹${Math.ceil(total)}`;
-})
-
 document.querySelector('#rupee').addEventListener('change', () => {
     invest_.value = document.querySelector('#rupee').value;
     document.querySelector('#rupee').value = `₹${invest_.value}`;
 })
 
+
+
+years_.oninput = () => {
+    document.querySelector('#years_val').value = `${years_.value} Years`;
+    document.querySelectorAll('#result h2')[0].innerHTML = `₹${(invest_.value) * (years_.value) * 12}`;
+
+    const total = invest_.value * (Math.pow(1 + percentage_.value / 1200, 12 * years_.value) - 1) / (percentage_.value / 1200);
+    document.querySelectorAll('#result h2')[1].innerHTML = `₹${Math.ceil(total)}`;
+}
+years_.addEventListener('mousemove', () => {
+    const x = years_.value;
+    years_.style.background = 'linear-gradient(90deg, rgb(208, 31, 75)' + x/0.3 + '%, rgb(191, 191, 191)' +  x/0.3 + '%)';
+})
 document.querySelector('#years_val').addEventListener('change', () => {
     years_.value = document.querySelector('#years_val').value;
     document.querySelector('#years_val').value = `${years_.value} Years`;
 })
 
+
+
+percentage_.oninput = () => {
+    document.querySelector('#percentage_val').value = `${percentage_.value}%`;
+    
+    const total = invest_.value * (Math.pow(1 + percentage_.value / 1200, 12 * years_.value) - 1) / (percentage_.value / 1200);
+    document.querySelectorAll('#result h2')[1].innerHTML = `₹${Math.ceil(total)}`;
+}
+percentage_.addEventListener('mousemove', () => {
+    const x = (percentage_.value/0.15 - 52) * 2;
+    percentage_.style.background = 'linear-gradient(90deg, rgb(208, 31, 75)' + x + '%, rgb(191, 191, 191)' +  x + '%)';
+})
 document.querySelector('#percentage_val').addEventListener('change', () => {
     percentage_.value = document.querySelector('#percentage_val').value;
     document.querySelector('#percentage_val').value = `${percentage_.value}%`;
